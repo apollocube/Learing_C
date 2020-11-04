51624373d1b689dd271dc09eb985ad733cfdeaf9
ollopa <55067757+apollocube@users.noreply.github.com>
2020-11-04 21:09:36 +0800

    Add files via upload

--- hello_world.c
+++ hello_world.c
@@ -1,10 +1,18 @@
 ﻿#include <stdio.h>
-int main()
-{
-	printf("hello, world!");
-	return 0;
-}
+#include "USF_CODES.h"
 
+#define MSG\
+ "you little son of beatch!"
+
+int main()
+{	
+	printf(MSG);
+	printf("\n%d",MAX_OF_TWO(2,3));
+	printf("57 is odd: %d, character z to upper is %c",IS_ODD(57),TO_UPPER('z'));
+	PRINT_INT(666+456);
+	return 0;
+	
+}
 
 
 
